f50115c21fc9a5bd7dbc4392f23cd30c40370c12
Felipe Peres <gpsouza.felipe@gmail.com>
2021-11-25 13:47:27 -0300

    Corrections

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  return response.json({repositories});
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
@@ -47,10 +47,6 @@ app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
   repositoryIndex = repositories.findIndex(repository => repository.id == id);
-
-  if(repositoryIndex < 0) {
-    return response.status(400).json({ error: 'Repository not found!'})
-  }
 
   repositories.splice(repositoryIndex, 1);
 
